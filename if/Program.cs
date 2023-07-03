060efc7010d4d536c64eee9715d87f5f8337d62f
Redlayne <redlayne@gmail.com>
2023-07-04 01:09:14 +0300

    ifadd

---
+++ if/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Who tf are you?");
+string username = Console.ReadLine();
+if(username.ToLower() == "dick" || username.ToLower() == "mick")
+{
+    Console.WriteLine("youre such a Dick");
+}
+else
+{
+    Console.WriteLine("hello, normal person");
+}
